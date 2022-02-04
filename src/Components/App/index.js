6d5a872e86056364b3ca821a056ef68b70a88a68
JoshuaMeah <joshmeah7@gmail.com>
2022-02-04 13:42:21 +0000

    navbar

--- src/Components/App/index.js
+++ src/Components/App/index.js
@@ -2,6 +2,7 @@ import "./App.css";
 import Input from "../Input";
 import { Login } from "../Login";
 import { useAuth0 } from "@auth0/auth0-react";
+import Navbar from "../navbar/header.js";
 
 function App() {
   const { isAuthenticated } = useAuth0();
@@ -10,6 +11,7 @@ function App() {
     <div className="App">
       {isAuthenticated ? (
         <main>
+          <Navbar />
           <Login />
           <h1>Welcome to the page</h1>
           <Input />
