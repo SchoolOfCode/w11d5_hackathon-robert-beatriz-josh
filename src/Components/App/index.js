3ec9527b1170a1b6961c59eaa8036bcebf1a3cd8
JoshuaMeah <joshmeah7@gmail.com>
2022-02-04 14:19:54 +0000

    api

--- src/Components/App/index.js
+++ src/Components/App/index.js
@@ -3,10 +3,23 @@ import Input from "../Input";
 import { Login } from "../Login";
 import { useAuth0 } from "@auth0/auth0-react";
 import Navbar from "../navbar/header.js";
+import { useEffect, useState } from "react";
 
 function App() {
   const { isAuthenticated } = useAuth0();
+  const [text, setText] = useState();
 
+  async function fetchGame() {
+    let data = await fetch(
+      `https://www.cheapshark.com/api/1.0/games?title=${text}`
+    );
+    let fetchInfo = await data.json;
+    setText(fetchInfo);
+    console.log(fetchInfo);
+  }
+  useEffect(() => {
+    fetchGame();
+  });
   return (
     <div className="App">
       <Navbar />
@@ -14,7 +27,7 @@ function App() {
         <main>
           <Login />
           <h1>Welcome to the page</h1>
-          <Input />
+          <Input onSubmit={fetchInfo} />
         </main>
       ) : (
         <Login />
--- src/Components/Input/index.js
+++ src/Components/Input/index.js
@@ -1,15 +1,20 @@
 import React, { useState } from "react";
 
-function Input() {
+function Input({ onSubmit }) {
   const [game, setGame] = useState("");
   function getValue(e) {
     setGame(e.target.value);
     console.log(game);
   }
+
+  function searchResults(e) {
+    e.preventDefault();
+    onSubmit(game);
+  }
   return (
     <form>
       <input placeholder="Search for a game" onChange={getValue}></input>
-      <button></button>
+      <button onClick={searchResults}>Search</button>
     </form>
   );
 }
