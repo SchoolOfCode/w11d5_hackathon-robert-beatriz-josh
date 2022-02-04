be6dc840f905cb00f42d59700927f3e45cee76f2
RobertHJones <roberthuwjones@outlook.com>
2022-02-04 16:36:00 +0000

    work?

--- src/Components/App/index.js
+++ src/Components/App/index.js
@@ -2,7 +2,6 @@ import "./App.css";
 import Input from "../Input";
 import { Login } from "../Login";
 import { useAuth0 } from "@auth0/auth0-react";
-import Navbar from "../navbar/header.js";
 import { useState } from "react";
 
 function App() {
@@ -12,7 +11,6 @@ function App() {
   async function fetchGame(game) {
     let response = await fetch(
       `https://www.cheapshark.com/api/1.0/games?title=${game}`
-      // `https://swapi.dev/api/people?search=${game}`
     );
     let data = await response.json();
     setText(data[0]);
@@ -21,14 +19,12 @@ function App() {
 
   return (
     <div className="App">
-      <Navbar />
       {isAuthenticated ? (
         <main>
           <Login />
           <h1>Welcome to the page</h1>
           <Input onSubmit={fetchGame} />
           <div>
-            {/* {/* <h2>{text.external}</h2> */}
             <h2>{text.external}</h2>
             <h3>The cheapest price for this is £{text.cheapest}</h3>
             <img src={text.thumb} alt="" />
